89b90706d3e6087a3e84e0d5386491a2ccb486e9
mateusrega <143010456+mateusrega@users.noreply.github.com>
2025-09-09 09:19:10 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    domains: ['firebasestorage.googleapis.com'],
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
